d81c24109b35c2fcd65f02895e28f5dd05f672b5
S.Ali Yaghoub Nejad <s.ali.yaghoubnejad@gmail.com>
2020-01-18 12:38:13 +0330

    process added ver.1

--- main.py
+++ main.py
@@ -1,4 +1,28 @@
 from queue import Queue 
+class Process :
+
+    def __init__(self, id, arrival_time, burst_time, io_time):
+        self.id = id
+        self.arrival_time = arrival_time
+        self.burst_time = burst_time
+        self.io_time = io_time
+        self.remaining_time = burst_time
+
+    def setStartTime(self, time):
+        self.start_time = time
+
+    def setTurnaroundTime(self, time):
+        self.turnaround_time = time
+
+    def getWaittingTime():
+        return self.turnaround_time - self.arrival_time - self.burst_time
+
+    def getResponseTime():
+        return self.start_time - self.arrival_time
+
+    def getTurnaroundTime():
+        return self.turnaround_time
+        
 class CPU:
     
     def __init__(self, all_process):
