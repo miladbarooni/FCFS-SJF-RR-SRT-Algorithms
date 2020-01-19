77616865b490e3b5e2c1ed3b05dcc3823e60785e
S.Ali Yaghoub Nejad <s.ali.yaghoubnejad@gmail.com>
2020-01-19 21:31:46 +0330

    Diagram was added

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from queue import Queue, PriorityQueue
 import threading
+import matplotlib.pyplot as plt
+from matplotlib.collections import PolyCollection
 
 class Process :
 
-    def __init__(self, id, arrival_time, burst_time, io_time):
-        self._id = id
+    def __init__(self, identity, arrival_time, burst_time, io_time):
+        self._identity = identity
         self._arrival_time = arrival_time
         self._burst_time = burst_time
         self._io_time = io_time
@@ -18,6 +20,14 @@ class Process :
     @property
     def burst_time(self):
         return self._burst_time
+
+    @property
+    def io_time(self):
+        return self._io_time
+
+    @property
+    def identity(self):
+        return self._identity
 
     def getRemainingTime(self):
         return self._remaining_time
@@ -45,12 +55,10 @@ class Process :
     def isFinished(self):
         return self._remaining_time == 0
 
-    def getID(self):
-        return self._id
 
     
     def __str__(self):
-        return 'id:{} arrival_time:{} burst_time:{} io_time:{}'.format(self.id, self.arrival_time, self.burst_time, self.io_time)
+        return 'identity:{} arrival_time:{} burst_time:{} io_time:{}'.format(self._identity, self._arrival_time, self._burst_time, self._io_time)
 
 class CPU: 
     
@@ -61,7 +69,7 @@ class CPU:
         self.waiting_queue = Queue(maxsize = len(all_process))
         # self.active_list = []
         for p in all_process:
-            self.all_proccess.append(Process(p.getID(), p.arrival_time, p.burst_time, p.getIOTime()))
+            self.all_proccess.append(Process(p.identity, p.arrival_time, p.burst_time, p.io_time))
 
     def FCFS(self):
         #ititial time and burst_time 
@@ -77,9 +85,9 @@ class CPU:
             p = self.ready_queue.get()
             if (p.arrival_time > self.time):
                 self.time = p.arrival_time
-            running_frame = [p.getID(), self.time]
+            running_frame = [self.time]
             self.time = p.run(self.time, p.burst_time)
-            running_frame.append(self.time)
+            running_frame += [self.time, "p" + '{}'.format(p.identity)]
             self._running_sequence.append(running_frame)
             self.total_burst_time += p.burst_time
 
@@ -97,6 +105,7 @@ class CPU:
         print ("Avg. R.T: ", self.average_response_time)
         print ("Throughput: ", self.throughput)
         print ("CPU Utilization: ", self.cpu_utilization)
+        return self.plot("FCFS")
 
     def SJF(self):
         #ititial time and burst_time 
@@ -124,10 +133,10 @@ class CPU:
                 continue
             else:
                 p = sorted_on_burst_time.pop()
-                runnig_frame = [p.getID(), self.time]
+                running_frame = [self.time]
                 self.time = p.run(self.time, p.burst_time)
-                runnig_frame.append(self.time)
-                self._running_sequence.append(runnig_frame)
+                running_frame += [self.time, "p" + '{}'.format(p.identity)]
+                self._running_sequence.append(running_frame)
                 self.total_burst_time += p.burst_time
 
         #printAllProcess()
@@ -146,6 +155,7 @@ class CPU:
         print ("Avg. R.T: ", self.average_response_time)
         print ("Throughput: ", self.throughput)
         print ("CPU Utilization: ", self.cpu_utilization)
+        return self.plot("SJF")
 
     def RR(self):
         sorted_on_arrival_time = Queue(maxsize=len(self.all_proccess))
@@ -171,15 +181,15 @@ class CPU:
             p = self.ready_queue.get()
                 
             if (p.getRemainingTime() > 5):
-                running_frame = [p.getID(), self.time]
+                running_frame = [self.time]
                 self.time = p.run(self.time, 5)
-                running_frame.append(self.time)
+                running_frame += [self.time, "p" + '{}'.format(p.identity)]
                 self._running_sequence.append(running_frame)
                 self.total_burst_time = self.total_burst_time + 5
             else:
-                running_frame = [p.getID(), self.time]
+                running_frame = [self.time]
                 self.time = p.run(self.time, p.getRemainingTime())
-                running_frame.append(self.time)
+                running_frame += [self.time, "p" + '{}'.format(p.identity)]
                 self._running_sequence.append(running_frame)
                 self.total_burst_time += p.getRemainingTime()
      
@@ -207,6 +217,7 @@ class CPU:
         print ("Avg. R.T: ", self.average_response_time)
         print ("Throughput: ", self.throughput)
         print ("CPU Utilization: ", self.cpu_utilization)
+        return self.plot("RR")
 
     def SRT(self):
         #ititial time and burst_time 
@@ -235,13 +246,13 @@ class CPU:
                 continue    
             else:
                 p = sorted_on_burst_time.pop()
-                running_frame = [p.getID(), self.time]
+                running_frame = [self.time]
                 self.time = p.run(self.time, 1)
-                running_frame.append(self.time)
-                if (len(self._running_sequence) == 0 or self._running_sequence[-1][0] != running_frame[0]):
+                running_frame += [self.time, "p" + '{}'.format(p.identity)]
+                if (len(self._running_sequence) == 0 or self._running_sequence[-1][2] != running_frame[2]):
                     self._running_sequence.append(running_frame)
                 else:
-                    self._running_sequence[-1][2] = running_frame[2]
+                    self._running_sequence[-1][1] = running_frame[1]
 
                 if (not p.isFinished()):
                     sorted_on_burst_time.append(p)
@@ -264,6 +275,7 @@ class CPU:
         print ("Avg. R.T: ", self.average_response_time)
         print ("Throughput: ", self.throughput)
         print ("CPU Utilization: ", self.cpu_utilization)
+        return self.plot("SRT")
 
     def printAllProcess(self) :
         # print all process attributes
@@ -279,6 +291,43 @@ class CPU:
     def getRunningSequence(self):
         return self._running_sequence
 
+    def plot(self, algo_name):
+        cats = {}
+        colormapping = {}
+        p_name = []
+        p_num = []
+        i = 0
+        for p in self.all_proccess:
+            cats["p" + '{}'.format(p.identity)] = i
+            colormapping["p" + '{}'.format(p.identity)] = "C" + '{}'.format(i % 10)
+            p_name.append("p" + '{}'.format(p.identity))
+            p_num.append(i)
+            i += 1
+
+        verts = []
+        colors = []
+        for d in self._running_sequence:
+            v =  [(d[0], cats[d[2]]-.45),
+                  (d[0], cats[d[2]]+.45),
+                  (d[1], cats[d[2]]+.45),
+                  (d[1], cats[d[2]]-.45),
+                  (d[0], cats[d[2]]-.45)]
+            verts.append(v)
+            colors.append(colormapping[d[2]])
+
+        bars = PolyCollection(verts, facecolors=colors)
+
+        fig, ax = plt.subplots()
+        fig.canvas.set_window_title(algo_name)
+        ax.set_xlabel("Time (ms)")
+        #ax.set_xlim(0,self.time)
+        plt.xticks([k for k in range(self.time + 1)])
+        ax.add_collection(bars)
+        ax.autoscale()
+
+        ax.set_yticks(p_num)
+        ax.set_yticklabels(p_name)
+        return plt
 
 
 process = []
@@ -293,15 +342,20 @@ cpu2 = CPU(process)
 cpu3 = CPU(process)
 cpu4 = CPU(process)
 
-FCFS = threading.Thread(cpu1.FCFS())
-SJF = threading.Thread(cpu2.SJF())
-RR = threading.Thread(cpu3.RR())
-SRT = threading.Thread(cpu4.SRT())
+SJF = threading.Thread(target = cpu2.SJF())
+FCFS = threading.Thread(target = cpu1.FCFS())
+RR = threading.Thread(target = cpu3.RR())
+SRT = threading.Thread(target = cpu4.SRT())
 
-FCFS.start()
 SJF.start()
+FCFS.start()
 RR.start()
 SRT.start()
+
+FCFS.join()
+SJF.join()
+RR.join()
+SRT.join()
 
 print("FCFS: ", cpu1.getRunningSequence())
 print("SJF: ", cpu2.getRunningSequence())
